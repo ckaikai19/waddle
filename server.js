48f987c7f08630e9e8e61eb0a4a53d52f665be73
chirstiankaikai <ckaikai19@gmail.com>
2022-07-24 19:09:17 -0400

    added begginging prompt

--- server.js
+++ server.js
@@ -1,3 +1,59 @@
 const inquirer = require('inquirer');
 const db_connection = require('./db/connection');
+require('console.table');
+
+connection.connect(() => {
+    console.log(`Welcome Employee Manager\n`)
+    firstPrompt();
+});
+
+function firstPrompt() {
+    inquirer
+        .prompt({
+            type: "list",
+            name: "task",
+            message: "Would you like to do?",
+            choices: [
+                "View Employees",
+                "View Employees by Department",
+                "Add Employee",
+                "Remove Employees",
+                "Update Employee Role",
+                "Add Role",
+                "End"]
+        })
+        .then(function ({ task }) {
+            switch (task) {
+                case "View Employees":
+                viewEmployee();
+                break;
+
+                case "View Employees by Department":
+                viewEmployeeByDepartment();
+                break;
+            
+                case "Add Employee":
+                addEmployee();
+                break;
+
+                case "Remove Employees":
+                removeEmployees();
+                break;
+
+                case "Update Employee Role":
+                updateEmployeeRole();
+                break;
+
+                case "Add Role":
+                addRole();
+                break;
+
+                case "End":
+                connection.end();
+                break;
+            }
+        });
+}
+
+
 
